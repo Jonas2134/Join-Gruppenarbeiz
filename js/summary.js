5888776ebf64a9fef8340541d2df16c76df30d5d
Jonas2134 <neo.stief.jon@gmail.com>
2024-06-05 00:34:19 +0200

    add summary.js and integrate tempates

---
+++ js/summary.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", async function () {
+    await includeHTML();
+    await loadCurrentUsers();
+
+    document.querySelector('.drop-logo').addEventListener('click', toggleDropdown);
+
+    window.addEventListener('click', function (event) {
+        if (!event.target.matches('.drop-logo')) {
+            let dropdowns = document.getElementsByClassName("dropdown-content");
+            for (let i = 0; i < dropdowns.length; i++) {
+                let openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    });
+});
