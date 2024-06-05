b98767349d4d18e843d2be61551e9d3c9c28fef8
unknown <jessicamanczak@gmx.de>
2024-06-06 00:00:18 +0200

    first design js sendTask

--- js/help_function.js
+++ js/help_function.js
@@ -2,7 +2,7 @@ async function includeHTML() {
   let includeElements = document.querySelectorAll('[w3-include-html]');
   for (let i = 0; i < includeElements.length; i++) {
     const element = includeElements[i];
-    file = element.getAttribute('w3-include-html'); 
+    file = element.getAttribute('w3-include-html');
     let resp = await fetch(file);
     if (resp.ok) {
       element.innerHTML = await resp.text();
@@ -40,7 +40,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
       if (draggableElement) {
         bar.appendChild(draggableElement);
 
-
         const noTaskElement = bar.querySelector('.no_task_to_do');
         if (noTaskElement) {
           noTaskElement.style.display = 'none';
@@ -48,7 +47,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
       }
       bar.style.backgroundColor = '';
 
-  
       taskBarContents.forEach((content) => {
         const noTaskElement = content.querySelector('.no_task_to_do');
         const taskCards = content.querySelectorAll('.task_card');
@@ -58,7 +56,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
       });
     });
   });
-
 
   taskCards.forEach((card) => {
     card.addEventListener('dragend', (event) => {
@@ -73,47 +70,41 @@ document.addEventListener('DOMContentLoaded', (event) => {
   });
 });
 
-
-
-
-
 //OVERLAY RIGHT FUNCTION
 function openOverlayRight() {
   let overlay = document.getElementById('add_task_overlay');
   if (overlay) {
-      overlay.style.display = 'flex';
-  } 
+    overlay.style.display = 'flex';
+  }
 }
 
 function closeOverlayRight() {
   let overlay = document.getElementById('add_task_overlay');
   if (overlay) {
-      overlay.style.display = 'none';
+    overlay.style.display = 'none';
   }
 }
 
-window.addEventListener('load', function() {
-  closeOverlayRight(); 
+window.addEventListener('load', function () {
+  closeOverlayRight();
 });
 
-window.addEventListener('beforeunload', function() {
-  closeOverlayRight(); 
+window.addEventListener('beforeunload', function () {
+  closeOverlayRight();
 });
 
-
-
-//OVERLAY TOP FUNCTION 
+//OVERLAY TOP FUNCTION
 function openOverlayTop() {
   let overlay = document.getElementById('card_top_overlay');
   if (overlay) {
-      overlay.style.display = 'flex';
-  } 
+    overlay.style.display = 'flex';
+  }
 }
 
 function closeOverlayTop() {
   let overlay = document.getElementById('card_top_overlay');
   if (overlay) {
-      overlay.style.display = 'none';
+    overlay.style.display = 'none';
   }
 }
 
@@ -131,51 +122,64 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
-
-
-
-
-
-
 //TOGGLE DROPDOWN FUNCTION
 function toggleDropdown() {
-  let content = document.getElementById("categoryDropdown");
-  let add_subtasks = document.getElementById("add_subtasks");
+  let content = document.getElementById('categoryDropdown');
+  let add_subtasks = document.getElementById('add_subtasks');
   if (content) {
-      if (content.classList.contains("show")) {
-          content.classList.remove("show");
-          add_subtasks.style.marginTop = "0px";
-      } else {
-          content.classList.add("show");
-          let dropdownHeight = content.offsetHeight;
-          console.log(dropdownHeight);
-          add_subtasks.style.marginTop = dropdownHeight + "px";
-      }
-  } 
+    if (content.classList.contains('show')) {
+      content.classList.remove('show');
+      add_subtasks.style.marginTop = '0px';
+    } else {
+      content.classList.add('show');
+      let dropdownHeight = content.offsetHeight;
+      add_subtasks.style.marginTop = dropdownHeight + 'px';
+    }
+  }
 }
 
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (!event.target.closest('.dropdown_header')) {
-      let dropdowns = document.getElementsByClassName("category_to_dropdown_content");
-      let add_subtasks = document.getElementById("add_subtasks");
-      for (i = 0; i < dropdowns.length; i++) {
-          let openDropdown = dropdowns[i];
-          if (openDropdown.classList.contains("show")) {
-              openDropdown.classList.remove("show");
-          }
+    let dropdowns = document.getElementsByClassName(
+      'category_to_dropdown_content'
+    );
+    let add_subtasks = document.getElementById('add_subtasks');
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
-      add_subtasks.style.marginTop = "0px";
+    }
+    add_subtasks.style.marginTop = '0px';
   }
 };
 
 // SELECT CATEGORY FUNCTION
 function selectCategory(text) {
-  let inputField = document.getElementById("addCategoryInputField");
+  let inputField = document.getElementById('addCategoryInputField');
   inputField.innerHTML = text;
 }
 
 
 
 
+//SELECT PRIORITY BUTTON
+function selectPriority(priority) {
+  let urgentPriority = document.getElementById('urgentPriority');
+  let mediumPriority = document.getElementById('mediumPriority');
+  let lowPriority = document.getElementById('lowPriority');
 
+  urgentPriority.classList.remove('priority_inactive', 'priority_active');
+  mediumPriority.classList.remove('priority_active', 'priority_inactive'); 
+  lowPriority.classList.remove('priority_active', 'priority_inactive');  
 
+  if (priority == 'urgent') { 
+  urgentPriority.classList.add('priority_active');
+  mediumPriority.classList.add('priority_inactive');
+  lowPriority.classList.add('priority_inactive');
+} else if (priority == 'medium') {
+
+} else if (priority == 'low') {
+  
+}
+}
--- js/storage.js
+++ js/storage.js
@@ -1,6 +1,18 @@
 const STORAGE_URL = "https://join-gruppenarbeit-c2942-default-rtdb.europe-west1.firebasedatabase.app/";
 const users = [];
 let currentUser = null;
+let tasks = [];
+
+async function updateData(path = "", data = {}) {
+    let response = await fetch(STORAGE_URL + path + ".json", {
+        method: "PUT",
+        header: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    });
+    return (responseToJson = await response.json());
+}
 
 async function postData(path = "", data = {}) {
     let response = await fetch(STORAGE_URL + path + ".json", {
@@ -13,6 +25,83 @@ async function postData(path = "", data = {}) {
     return (responseToJson = await response.json());
 }
 
+
+async function sendTask() {
+let task = {
+    id: null,
+    title: '',
+    description: '',
+    dueDate: '',
+    priority: '',
+    assignedTo: [],
+    content: '',
+    subtasks: [],
+    status: 'To do'
+};
+
+let taskTitle = document.getElementById('taskTitle').value;
+let taskDescription = document.getElementById('taskDescription').value;
+let taskDueDate = document.getElementById('taskDueDate').value;
+let taskPriority = 'medium'; 
+if(document.getElementById('urgentPriority').classList.contains('priority_active')) {
+   taskPriority = 'urgent';
+} else if (document.getElementById('mediumPriority').classList.contains('priority_active')) {
+   taskPriority = 'medium';
+} else if (document.getElementById('lowPriority').classList.contains('priority_active')) {
+   taskPriority = 'low'; 
+}
+let taskAssignedTo = '';
+let taskContentCategory = document.getElementById('addCategoryInputField').innerHTML;
+let taskSubtask = '';
+
+task.title = taskTitle;
+task.description = taskDescription;
+task.dueDate = taskDueDate;
+task.priority = taskPriority;
+task.assignedTo = taskAssignedTo;
+task.content = taskContentCategory;
+task.subtasks = taskSubtask;
+    // maxID oops
+/* let incMaxId = await getMaxTaskId() + 1;
+let path = '/tasks/' + incMaxId + '/task';
+task.id = incMaxId;
+await postData(path, task);
+await setMaxTaskId(incMaxId); */
+postData('/tasks', task);
+}
+
+function renderAllTasks() {
+    let currentTask = document.getElementById('drag_to_do');
+    currentTask.innerHTML = '';
+    for (let i = 0; i < tasks.length; i++) {
+
+        currentTask.innerHTML += renderTask(tasks[i]);
+                
+    }
+}
+
+function renderTask(task) {
+    return `
+    <div draggable="true"> ${task.title} </div>
+    `;
+
+
+}
+
+
+
+
+async function showTasks() {
+    await loadTasks();
+    renderAllTasks();
+      }
+
+
+
+
+
+
+
 async function getData(path = "") {
     let response = await fetch(STORAGE_URL + path + ".json");
     return (responseToJson = await response.json());
@@ -24,6 +113,56 @@ async function deleteData(path = "") {
     });
     return (responseToJson = await response.json());
 }
+
+async function loadTasks() {
+    // maxID oops
+/*     let maxTaskId = await getMaxTaskId();
+    console.log(maxTaskId);
+    let currentTask = '';
+    for (let index = 0; index <= maxTaskId; index++) {
+        currentTask = await getData('/task/' + index + '/task');
+        console.log(currentTask);
+        for (const key in currentTask) {
+            if (Object.hasOwnProperty.call(currentTask, key)) {
+                tasks.push(currentTask[key]);
+            }
+        }        
+    } */
+    let allTask = await getData('/tasks');
+    for (const key in allTask) {
+        if (Object.hasOwnProperty.call(allTask, key)) {
+            tasks.push(allTask[key]);
+        }
+    }   
+}
+
+// Funktion zum Testdaten resetten
+// maxID oops
+/* async function resetMaxTaskId() {
+    let json = {
+        maxTaskId: 0
+    };
+    console.log(json);
+    await deleteData('/maxTask');
+    await postData('/maxTask', json);
+}
+
+async function setMaxTaskId(id) {
+    let json = {
+        maxTaskId: 0
+    }
+    json.maxTaskId = id;
+    updateData('/maxTask', json);
+}
+
+async function getMaxTaskId() {
+    let idJson = await getData('/maxTask');
+    for (const key in idJson) {
+        if (Object.hasOwnProperty.call(idJson, key)) {
+            return(idJson[key].maxTaskId);
+        }
+    }
+} */
 
 async function loadUsers() {
     let loadedUsers = await getData("/users");
@@ -42,3 +181,7 @@ async function loadCurrentUsers() {
         }
     }
 }
+
+
+
+
