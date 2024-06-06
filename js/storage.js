df95e8ea6130f5dddf54642a12788b2248700434
Jonas2134 <neo.stief.jon@gmail.com>
2024-06-06 17:08:32 +0200

    Add showDropUser and renderGreeting

--- js/storage.js
+++ js/storage.js
@@ -43,39 +43,19 @@ async function loadCurrentUsers() {
     }
 }
 
-function populateRememberMe() {
-    const userData = getCookie("currentUser");
-    if (userData) {
-        const user = JSON.parse(userData);
-        document.getElementById("user_email").value = user.email;
-        document.getElementById("user_password").value = user.password;
-        document.getElementById("remember_me").checked = true;        
-    }
-}
+function showDropUser() {
+    const name = currentUser.user_name.split(" ");
+    let initials;
 
-function setCookie(name, value, days) {
-    let expires = "";
-    if (days) {
-        let date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
+    if (name.length === 1) {
+        initials = name[0].charAt(0).toUpperCase();
+    } else {
+        const firstNameLetter = name[0].charAt(0).toUpperCase();
+        const lastNameLetter = name[name.length - 1].charAt(0).toUpperCase();
+        initials = firstNameLetter + lastNameLetter;
     }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
-}
 
-function getCookie(name) {
-    const nameEQ = name + "=";
-    const ca = document.cookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-        const c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-}
-
-function eraseCookie(name) {
-    document.cookie = name + '=; Max-Age=-99999999;';
+    document.getElementById("drop_user").innerHTML = initials;
 }
 
 async function logOut() {
--- js/summary.js
+++ js/summary.js
@@ -1,7 +1,17 @@
+function renderGreeting() {
+    const greetingElement = document.getElementById('good_morning');
+    if (currentUser.user_name === "Guest User") {
+        greetingElement.textContent = "Good morning";
+    } else {
+        greetingElement.innerHTML = `Good morning, <span class="blue_name">${currentUser.user_name}</span>`;
+    }
+}
+
 document.addEventListener("DOMContentLoaded", async function () {
     await includeHTML();
     await loadCurrentUsers();
-
+    showDropUser();
+    renderGreeting();
 
     document.getElementById("log_out").addEventListener('click', logOut)
 
