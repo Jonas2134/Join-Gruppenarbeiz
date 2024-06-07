23da79d58f551c3c9d9261f769e1c874efc431a7
Johannes-M-Schaefer <163585576+Johannes-M-Schaefer@users.noreply.github.com>
2024-06-07 06:46:02 +0200

    posting contacts on server

---
+++ js/contacts.js
@@ -0,0 +1,33 @@
+function renderContacts() {
+    for (let i = 0; i < contacts.length; i++) {
+        const contact = contacts[i];
+        console.log(contact);
+    }
+}
+
+function addContact() {
+        let name = document.getElementById('name').value;
+        let email = document.getElementById('email').value;
+        let mobile = document.getElementById('mobile').value;
+    
+        let contact = {
+            name: name,
+            email: email,
+            mobile: mobile,
+        };
+    
+        console.log('contact', contact);
+        clearForm();
+        postData("/contacts", contact);
+}
+
+function clearForm() {
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('mobile').value = '';
+}
+
+function openAddContactOverlay() {
+    let addContactOverlay = document.getElementById("overlay_add-contact");
+    addContactOverlay.style.display = "flex";
+}
--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,54 @@
-const STORAGE_URL = "https://join-gruppenarbeit-85792-default-rtdb.europe-west1.firebasedatabase.app/";
+const STORAGE_URL = "https://join-gruppenarbeit-c2942-default-rtdb.europe-west1.firebasedatabase.app/";
+const users = [];
+let currentUser = null;
+const contacts = [];
+
+async function postData(path = "", data = {}) {
+    let response = await fetch(STORAGE_URL + path + ".json", {
+        method: "POST",
+        header: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    });
+    return (responseToJson = await response.json());
+}
+
+async function getData(path = "") {
+    let response = await fetch(STORAGE_URL + path + ".json");
+    return (responseToJson = await response.json());
+}
+
+async function deleteData(path = "") {
+    let response = await fetch(STORAGE_URL + path + ".json", {
+        method: "DELETE",
+    });
+    return (responseToJson = await response.json());
+}
+
+async function loadUsers() {
+    let loadedUsers = await getData("/users");
+    for (const key in loadedUsers) {
+        if (Object.hasOwnProperty.call(loadedUsers, key)) {
+            users.push(loadedUsers[key]);
+        }
+    }
+}
+
+async function loadContacts() {
+    let loadedContacts = await getData("/contacts");
+    for (const key in loadedContacts) {
+        if (Object.hasOwnProperty.call(loadedContacts, key)) {
+            contacts.push(loadedContacts[key]);
+        }
+    }
+}
+
+async function loadCurrentUsers() {
+    let loadedCurrentUser = await getData("/currentUser");
+    for (const key in loadedCurrentUser) {
+        if (Object.hasOwnProperty.call(loadedCurrentUser, key)) {
+            currentUser = loadedCurrentUser[key];
+        }
+    }
+}
