42a56b64e6e4a63bc502b6c6c483c434199fdec5
unknown <jessicamanczak@gmx.de>
2024-06-15 19:50:10 +0200

    added template.js added function categorys

--- js/board.js
+++ js/board.js
@@ -1,235 +1,146 @@
 //OVERLAY TOP FUNCTION
 function openOverlayTop() {
-    let overlay = document.getElementById('card_top_overlay');
-    if (overlay) {
-      overlay.style.display = 'flex';
-      setTimeout(() => {
-        overlay.classList.add('show');
-      }, 10); 
-    }
+  let overlay = document.getElementById('card_top_overlay');
+  if (overlay) {
+    overlay.style.display = 'flex';
+    setTimeout(() => {
+      overlay.classList.add('show');
+    }, 10);
   }
-  
-  function closeOverlayTop() {
-    let overlay = document.getElementById('card_top_overlay');
-    if (overlay) {
-      overlay.classList.remove('show');
-      overlay.addEventListener('transitionend', function handleTransitionEnd() {
-        if (!overlay.classList.contains('show')) {
-          overlay.style.display = 'none';
-          overlay.removeEventListener('transitionend', handleTransitionEnd);
-        }
-      });
-    }
-  }
-  
-  document.addEventListener('DOMContentLoaded', () => {
-    const cardOverlay = document.getElementById('card_top_overlay');
-    const overlayContent = document.getElementById('overlay_top_content');
-  
-    if (cardOverlay && overlayContent) {
-      cardOverlay.addEventListener('click', (event) => {
-        // Überprüfen, ob der Klick nicht auf das Overlay-Inhaltsbereich erfolgt
-        if (!overlayContent.contains(event.target)) {
-          closeOverlayTop();
-        }
-      });
-    }
-  });
-
-
-  function buildOverlayCard(i) {
-   let content = document.getElementById('overlay_top_content');
-  let task = tasks[i];
-  let title = task['title'];
-
-
-    content.innerHTML = 
-    `
-  <div class="overlay_card">
-
-      <div class="overlay_category">
-        <span class="overlay_category_span">${task.content}</span>   
-        <img src="./icons/add_task_escape_img.png" alt="close"
-                class="add_task_escape_img">    
-      </div>
-    <div class="overlay_title">
-      <span class="overlay_title_span"><b>${task.title}</b></span>
-    </div>
-      <div class="overlay_description">
-        <span>${task.description}</span>
-      </div>
-    <div class="overlay_date">
-      <span><b>Due date:</b>${task.dueDate}</span>
-    </div>
-      <div class="overlay_priority">
-        <span><b>Priority:</b>${task.priority}</span>  
-      </div>
-    <div class="overlay_contacts">
-      <span><b>Assigned To:</b></span>
-      <p id="overlayContactsRender">
-      ${task.assignedTo}
-      </p>
-    </div>
-      <div class="overlay_subtasks">
-        <span><b>Subtasks</b></span>  
-        <p><input type="checkbox"> ${task.subtasks}</p>
-        <p><input type="checkbox"> ${task.subtasks}</p>
-      </div>
-  </div>
-
-      <div class="overlay_icons">
-  <img src="/icons/delete_icon.png" alt="delete" class="overlay_icon_delete">
-    <span>Delete</span>
-  <div class="subtask_divider"></div>
-  <img src="/icons/edit_icon.png" alt="edit" class="overlay_edit_icon">
-    <span>Edit</span>
-      </div>
-  
-    `;
-
-  }
-  
-
-  function templateBuildContactList(contact) {
-    let contactName = contact['name'];
-    let contactId = contact['id'];
-    let initials = templateUserInitials(contact);
-  
-    return `
-    <li onclick="selectContact('${contactId}')" class="" id="${contactId}">
-          ${initials}${contactName}</li>
-          `;
-  }
-
-
-  //OVERLAY RIGHT FUNCTION
-function openOverlayRight() {
-    let overlay = document.getElementById('add_task_overlay');
-    if (overlay) {
-      overlay.style.display = 'flex';
-      setTimeout(() => {
-        overlay.classList.add('show');
-      }, 10); 
-    }
-  }
-  
-  function closeOverlayRight() {
-    let overlay = document.getElementById('add_task_overlay');
-    if (overlay) {
-      overlay.classList.remove('show');
-      overlay.addEventListener('transitionend', function handleTransitionEnd() {
-        if (!overlay.classList.contains('show')) {
-          overlay.style.display = 'none';
-          overlay.removeEventListener('transitionend', handleTransitionEnd);
-        }
-      });
-    }
-  }
-  
-  window.addEventListener('load', function () {
-    closeOverlayRight();
-  });
-  
-  window.addEventListener('beforeunload', function () {
-    closeOverlayRight();
-  });
-  
-
-  // DRAG & DROP FUNCTION
-document.addEventListener('DOMContentLoaded', (event) => {
-    const taskCards = document.querySelectorAll('.task_card');
-    const taskBarContents = document.querySelectorAll('.task_bar_content');
-  
-    taskCards.forEach((card) => {
-      card.addEventListener('dragstart', (event) => {
-        event.dataTransfer.setData('text/plain', event.target.id);
-      });
-    });
-  
-    taskBarContents.forEach((bar) => {
-      bar.addEventListener('dragover', (event) => {
-        event.preventDefault();
-        bar.style.backgroundColor = '#4589ff';
-      });
-  
-      bar.addEventListener('dragleave', (event) => {
-        bar.style.backgroundColor = '';
-      });
-  
-      bar.addEventListener('drop', (event) => {
-        event.preventDefault();
-        const id = event.dataTransfer.getData('text');
-        const draggableElement = document.getElementById(id);
-        if (draggableElement) {
-          bar.appendChild(draggableElement);
-  
-          const noTaskElement = bar.querySelector('.no_task_to_do');
-          if (noTaskElement) {
-            noTaskElement.style.display = 'none';
-          }
-        }
-        bar.style.backgroundColor = '';
-  
-        taskBarContents.forEach((content) => {
-          const noTaskElement = content.querySelector('.no_task_to_do');
-          const taskCards = content.querySelectorAll('.task_card');
-          if (noTaskElement && taskCards.length === 0) {
-            noTaskElement.style.display = 'flex';
-          }
-        });
-      });
-    });
-  
-    taskCards.forEach((card) => {
-      card.addEventListener('dragend', (event) => {
-        taskBarContents.forEach((content) => {
-          const noTaskElement = content.querySelector('.no_task_to_do');
-          const taskCards = content.querySelectorAll('.task_card');
-          if (noTaskElement && taskCards.length === 0) {
-            noTaskElement.style.display = 'flex';
-          }
-        });
-      });
-    });
-  });
-  
-  function renderAllTasks() {
-    let currentTask = document.getElementById('drag_to_do');
-    for (let i = 0; i < tasks.length; i++) {
-        currentTask.innerHTML += renderTask(tasks[i], i);                
-    }
 }
 
-function renderTask(task, i) {
-    return `
-    <div draggable="true" id="${task['ID']}" class="card_complete" onclick="buildOverlayCard(${i}), openOverlayTop()"> 
-      <div class="card_category_user_story">
-    <span class="card_category_user_story_span">${task.content}</span>
-      </div>
-    <div class="card_top_section">
-        <div class="card_title">
-    <b>${task.title}</b>
-        </div>
-      <div class="card_description">
-    ${task.description}
-      </div>
-    </div>
-        <div class="card_task_status">
-    1/2
-        </div>
-    <div class="card_bottom_section">    
-      <div class="card_contacts">
-    ${task.assignedTo}
-      </div>
-        <div class="card_prio">
-    ${task.priority}
-        </div>
-    </div>
-      </div>
-    `;
+function closeOverlayTop() {
+  let overlay = document.getElementById('card_top_overlay');
+  if (overlay) {
+    overlay.classList.remove('show');
+    overlay.addEventListener('transitionend', function handleTransitionEnd() {
+      if (!overlay.classList.contains('show')) {
+        overlay.style.display = 'none';
+        overlay.removeEventListener('transitionend', handleTransitionEnd);
+      }
+    });
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const cardOverlay = document.getElementById('card_top_overlay');
+  const overlayContent = document.getElementById('overlay_top_content');
+  if (cardOverlay && overlayContent) {
+    cardOverlay.addEventListener('click', (event) => {
+      if (!overlayContent.contains(event.target)) {
+        closeOverlayTop();
+      }
+    });
+  }
+});
+
+//OVERLAY RIGHT FUNCTION
+function openOverlayRight() {
+  let overlay = document.getElementById('add_task_overlay');
+  if (overlay) {
+    overlay.style.display = 'flex';
+    setTimeout(() => {
+      overlay.classList.add('show');
+    }, 10);
+  }
+}
+
+function closeOverlayRight() {
+  let overlay = document.getElementById('add_task_overlay');
+  if (overlay) {
+    overlay.classList.remove('show');
+    overlay.addEventListener('transitionend', function handleTransitionEnd() {
+      if (!overlay.classList.contains('show')) {
+        overlay.style.display = 'none';
+        overlay.removeEventListener('transitionend', handleTransitionEnd);
+      }
+    });
+  }
+}
+
+window.addEventListener('load', function () {
+  closeOverlayRight();
+});
+
+window.addEventListener('beforeunload', function () {
+  closeOverlayRight();
+});
+
+//RENDER TASKS ON BOARD
+function renderAllTasks() {
+  let currentTask = document.getElementById('drag_to_do');
+  for (let i = 0; i < tasks.length; i++) {
+    currentTask.innerHTML += renderTask(tasks[i], i);
+  }
 }
 
 async function showTasks() {
-    await loadTasks();
-    renderAllTasks();
+  await loadTasks();
+  renderAllTasks();
+}
+
+//BUILD OVERLAY TASK CARD
+function buildOverlayCard(i) {
+  let content = document.getElementById('overlay_top_content');
+  let task = tasks[i];
+  content.innerHTML = templateBuildOverlayCard(task);
+}
+
+
+// DRAG & DROP FUNCTION
+document.addEventListener('DOMContentLoaded', (event) => {
+  const taskCards = document.querySelectorAll('.task_card');
+  const taskBarContents = document.querySelectorAll('.task_bar_content');
+
+  taskCards.forEach((card) => {
+    card.addEventListener('dragstart', (event) => {
+      event.dataTransfer.setData('text/plain', event.target.id);
+    });
+  });
+
+  taskBarContents.forEach((bar) => {
+    bar.addEventListener('dragover', (event) => {
+      event.preventDefault();
+      bar.style.backgroundColor = '#4589ff';
+    });
+
+    bar.addEventListener('dragleave', (event) => {
+      bar.style.backgroundColor = '';
+    });
+
+    bar.addEventListener('drop', (event) => {
+      event.preventDefault();
+      const id = event.dataTransfer.getData('text');
+      const draggableElement = document.getElementById(id);
+      if (draggableElement) {
+        bar.appendChild(draggableElement);
+
+        const noTaskElement = bar.querySelector('.no_task_to_do');
+        if (noTaskElement) {
+          noTaskElement.style.display = 'none';
+        }
       }
+      bar.style.backgroundColor = '';
+
+      taskBarContents.forEach((content) => {
+        const noTaskElement = content.querySelector('.no_task_to_do');
+        const taskCards = content.querySelectorAll('.task_card');
+        if (noTaskElement && taskCards.length === 0) {
+          noTaskElement.style.display = 'flex';
+        }
+      });
+    });
+  });
+
+  taskCards.forEach((card) => {
+    card.addEventListener('dragend', (event) => {
+      taskBarContents.forEach((content) => {
+        const noTaskElement = content.querySelector('.no_task_to_do');
+        const taskCards = content.querySelectorAll('.task_card');
+        if (noTaskElement && taskCards.length === 0) {
+          noTaskElement.style.display = 'flex';
+        }
+      });
+    });
+  });
+});
--- js/task.js
+++ js/task.js
@@ -3,56 +3,39 @@ let subtasks = [];
 
 //SEND TASK TO FIREBASE
 async function sendTask() {
-  let task = {
-    title: '',
-    description: '',
-    dueDate: '',
-    priority: '',
-    assignedTo: [],
-    content: '',
-    subtasks: [],
+    let task = {
+    title: document.getElementById('taskTitle').value,
+    description: document.getElementById('taskDescription').value,
+    dueDate: document.getElementById('taskDueDate').value,
+    priority: getPriority(),
+    assignedTo: assignedContacts,
+    content: document.getElementById('addCategoryInputField').innerHTML,
+    subtasks: subtasks,
     status: 'To do',
   };
-
-  let taskTitle = document.getElementById('taskTitle').value;
-  let taskDescription = document.getElementById('taskDescription').value;
-  let taskDueDate = document.getElementById('taskDueDate').value;
-  let taskPriority = 'Medium';
-  if (
-    document
-      .getElementById('urgentPriority')
-      .classList.contains('priority_active')
-  ) {
-    taskPriority = 'Urgent';
-  } else if (
-    document
-      .getElementById('mediumPriority')
-      .classList.contains('priority_active')
-  ) {
-    taskPriority = 'Medium';
-  } else if (
-    document.getElementById('lowPriority').classList.contains('priority_active')
-  ) {
-    taskPriority = 'Low';
-  }
-  let taskContentCategory = document.getElementById(
-    'addCategoryInputField'
-  ).innerHTML;
-
-  task.title = taskTitle;
-  task.description = taskDescription;
-  task.dueDate = taskDueDate;
-  task.priority = taskPriority;
-  task.assignedTo = assignedContacts;
-  task.content = taskContentCategory;
-  task.subtasks = subtasks;
-
   postData('/tasks', task);
   clearAddTask();
 }
 
 //SELECT PRIORITY BUTTON
 function selectPriority(priority) {
+  clearPriority(false);
+  if (priority == 'urgent') {
+    urgentPriority.classList.add('priority_active');
+    mediumPriority.classList.add('priority_inactive');
+    lowPriority.classList.add('priority_inactive');
+    document.getElementById('prioRed').src = 'icons/prio_red_white.png';
+  } else if (priority == 'medium') {
+    clearPriority(true);
+  } else if (priority == 'low') {
+    lowPriority.classList.add('priority_active');
+    urgentPriority.classList.add('priority_inactive');
+    mediumPriority.classList.add('priority_inactive');
+    document.getElementById('prioGreen').src = 'icons/prio_green_white.png';
+  }
+}
+
+function clearPriority(reset) {
   let urgentPriority = document.getElementById('urgentPriority');
   let mediumPriority = document.getElementById('mediumPriority');
   let lowPriority = document.getElementById('lowPriority');
@@ -60,28 +43,31 @@ function selectPriority(priority) {
   urgentPriority.classList.remove('priority_inactive', 'priority_active');
   mediumPriority.classList.remove('priority_active', 'priority_inactive');
   lowPriority.classList.remove('priority_active', 'priority_inactive');
+  document.getElementById('prioRed').src = 'icons/prio_red.png';
+  document.getElementById('prioOrange').src = 'icons/prio_orange.png';
+  document.getElementById('prioGreen').src = 'icons/prio_green.png';
 
-  if (priority == 'urgent') {
-    urgentPriority.classList.add('priority_active');
-    mediumPriority.classList.add('priority_inactive');
-    lowPriority.classList.add('priority_inactive');
-    document.getElementById('prioRed').src = 'icons/prio_red_white.png';
-    document.getElementById('prioOrange').src = 'icons/prio_orange.png';
-    document.getElementById('prioGreen').src = 'icons/prio_green.png';
-  } else if (priority == 'medium') {
+  if (reset) {
     mediumPriority.classList.add('priority_active');
     urgentPriority.classList.add('priority_inactive');
     lowPriority.classList.add('priority_inactive');
-    document.getElementById('prioRed').src = 'icons/prio_red.png';
     document.getElementById('prioOrange').src = 'icons/prio_orange_white.png';
-    document.getElementById('prioGreen').src = 'icons/prio_green.png';
-  } else if (priority == 'low') {
-    lowPriority.classList.add('priority_active');
-    urgentPriority.classList.add('priority_inactive');
-    mediumPriority.classList.add('priority_inactive');
-    document.getElementById('prioRed').src = 'icons/prio_red.png';
-    document.getElementById('prioOrange').src = 'icons/prio_orange.png';
-    document.getElementById('prioGreen').src = 'icons/prio_green_white.png';
+  }
+}
+
+function getPriority() {
+  if (
+    document.getElementById('urgentPriority').classList.contains('priority_active')
+  ) {
+    return 'Urgent';
+  } else if (
+    document.getElementById('mediumPriority').classList.contains('priority_active')
+  ) {
+    return 'Medium';
+  } else if (
+    document.getElementById('lowPriority').classList.contains('priority_active')
+  ) {
+    return 'Low';
   }
 }
 
@@ -122,12 +108,9 @@ function toggleCategoryDropdown() {
 //ADD CONTACTS
 async function addTaskContacs() {
   await initContacts();
-
   for (i = 0; i < contacts.length; i++) {
     const contact = contacts[i];
-
     let contactDropwdown = document.getElementById('assignedDropdown');
-
     contactDropwdown.innerHTML += templateBuildContactDropdown(contact);
   }
 }
@@ -136,7 +119,6 @@ function selectContact(contactId) {
   let assignedContact = document.getElementById(contactId);
   let index = assignedContacts.indexOf(contactId);
   let checkbox = document.getElementById('cb' + contactId);
-
   if (index == -1) {
     assignedContacts.push(contactId);
     assignedContact.classList.add('active');
@@ -176,9 +158,7 @@ function selectCategory(text) {
 function addSubtask() {
   let subtaskInput = document.getElementById('inputSubtasks');
   let subtask = subtaskInput.value.trim();
-
   if (subtask === '') return;
-
   subtasks.push(subtask);
   renderSubtasks();
   clearInput('inputSubtasks');
@@ -189,7 +169,6 @@ function addSubtask() {
 function renderSubtasks() {
   let subtaskContent = document.getElementById('subtaskContent');
   subtaskContent.innerHTML = '';
-
   subtasks.forEach((subtask, index) => {
     subtaskContent.innerHTML += templateBuildSubtask(subtask, index);
   });
@@ -205,7 +184,6 @@ function deleteSubtask(index) {
 function editSubtask(index) {
   let subTaskContent = document.getElementById(`subtask_${index}`);
   let subTaskText = document.getElementById(`subtask_edit_${index}`);
-
   subTaskContent.classList.add('inactive');
   subTaskText.classList.remove('inactive');
 }
@@ -214,7 +192,6 @@ function onInputSubtask() {
   let subtaskInput = document.getElementById('inputSubtasks').value;
   let subtaskIconActive = document.getElementById('subtaskIconsActive');
   let subtaskIconInactive = document.getElementById('subtaskIconsInactive');
-
   if (subtaskInput.trim() !== '') {
     subtaskIconActive.classList.add('inactive');
     subtaskIconInactive.classList.remove('inactive');
@@ -227,10 +204,8 @@ function onInputSubtask() {
 //SAVE SUBTASKS
 function saveSubtask(index) {
   let subtaskInput = document.getElementById(`subtask_input_${index}`).value;
-
   if (subtaskInput !== null && subtaskInput.trim() !== '') {
     subtasks[index] = subtaskInput.trim();
-
     renderSubtasks();
   }
 }
@@ -248,69 +223,12 @@ function clearAddTask() {
   clearInput('taskTitle');
   clearInput('taskDescription');
   clearInput('taskDueDate');
-  clearInput('inputSubtasks');
-
-  document.getElementById('mediumPriority').classList.add('priority_active');
-  document
-    .getElementById('mediumPriority')
-    .classList.remove('priority_inactive');
-  document.getElementById('urgentPriority').classList.remove('priority_active');
-  document.getElementById('urgentPriority').classList.add('priority_inactive');
-  document.getElementById('lowPriority').classList.remove('priority_active');
-  document.getElementById('lowPriority').classList.add('priority_inactive');
+  clearPriority(true);
   clearHtml('selectAssignedTo', `Select contacts to assign`);
   assignedContacts = [];
   renderAssignedContact();
   clearHtml('addCategoryInputField', `Select task category`);
+  clearInput('inputSubtasks');
   clearHtml('subtaskContent', '');
   subtasks = [];
-}
-
-//TEMPLATE ADD TASKS
-function templateBuildContactDropdown(contact) {
-  let contactName = contact['name'];
-  let contactId = contact['id'];
-  let initials = templateUserInitials(contact);
-
-  return `
-        <li onclick="selectContact('${contactId}')" class="" id="${contactId}">
-        ${initials}${contactName}
-         </div>
-        <div>
-        <input class="custom_checkbox" type="checkbox" id="cb${contactId}" onclick="event.stopPropagation();">
-        </li>
-        </div>
-        `;
-}
-
-function templateUserInitials(contact) {
-  return `
-  <div class="svg_contacts_name_div">
-    <svg width="36" height="36">
-        <circle cx="18" cy="18" r="15" fill="${contact.color}" />
-        <text x="8" y="24" font-size="1rem" fill="#ffffff">${contact.initials}</text>
-    </svg>
-   
-  `;
-}
-
-function templateBuildSubtask(subtask, index) {
-  return `
-  <div class="build_subtask" id="subtask_${index}">
-  <li class="build_subtask_span">${subtask}</li>
-  <div class="subtask_icons_div">
-  <img src="/icons/edit_icon.png" alt="edit" class="subtask_icon" onclick="editSubtask(${index})">
-  <div class="subtask_divider"></div>
-  <img src="/icons/delete_icon.png" alt="delete" class="subtask_icon" onclick="deleteSubtask(${index})">
-  </div>
-  </div>
-  <div class="build_subtask_2 inactive" id="subtask_edit_${index}">
-  <input class="build_subtask_span_2" value="${subtask}" id="subtask_input_${index}"></input>
-  <div class="subtask_icons_div">
-  <img src="/icons/delete_icon.png" alt="delete" class="subtask_icon_delete" onclick="deleteSubtask(${index})">
-  <div class="subtask_divider"></div>
-  <img class="subtask_check_icon" src="icons/check.png" onclick="saveSubtask(${index})">
-  </div>
-  </div>
-  `;
 }
