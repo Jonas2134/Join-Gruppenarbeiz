e0e100d9dbe3b6b169ba78a799b806a5ae656a88
Johannes-M-Schaefer <163585576+Johannes-M-Schaefer@users.noreply.github.com>
2024-06-27 17:13:13 +0200

    validate inputs and height responsive overlay

--- js/contacts.js
+++ js/contacts.js
@@ -129,7 +129,7 @@ function showCreationPopup() {
     docID('creation_popup').classList.add('show');
     setTimeout(() => {
         docID('creation_popup_container').classList.remove('show');
-    }, 2000);
+    }, 1000);
 }
 
 function showUpdatePopup() {
@@ -137,7 +137,7 @@ function showUpdatePopup() {
     docID('update_popup').classList.add('show');
     setTimeout(() => {
         docID('update_popup_container').classList.remove('show');
-    }, 2000);
+    }, 1000);
 }
 
 function showDeletePopup() {
@@ -145,7 +145,7 @@ function showDeletePopup() {
     docID('delete_popup').classList.add('show');
     setTimeout(() => {
         docID('delete_popup_container').classList.remove('show');
-    }, 2000);
+    }, 1000);
 }
 
 function clearAddForm() {
@@ -159,7 +159,7 @@ function clearEditForm() {
 function openAddContactOverlay() {
     let contactOverlay = docID("overlay_add-contact");
     contactOverlay.classList.remove('d-none');
-    
+    activateInputError();
 }
 
 function openEditContactOverlay(i) {
@@ -209,120 +209,62 @@ function openContact(i) {
     let selectedContainer = docID('selected-container');
 
     selectedContainer.innerHTML = generateContactHTML(i);
-    
-    /* `
-        <div class="selected-contact-main-container">
-            <div class="open-nav-button d-none" onclick="showNav()">
-                <div class="add-contact-button-text">Add new contact</div>
-                <div class="add-contact-button-img">
-                    <svg width="33" height="32" viewBox="0 0 33 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <mask id="mask0_45731_2256" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="33" height="32">
-                            <rect x="0.21875" width="32" height="32" fill="#D9D9D9"/>
-                        </mask>
-                        <g mask="url(#mask0_45731_2256)">
-                            <path d="M16.2184 26.6666C15.4851 26.6666 14.8573 26.4055 14.3351 25.8833C13.8129 25.361 13.5518 24.7333 13.5518 23.9999C13.5518 23.2666 13.8129 22.6388 14.3351 22.1166C14.8573 21.5944 15.4851 21.3333 16.2184 21.3333C16.9518 21.3333 17.5795 21.5944 18.1018 22.1166C18.624 22.6388 18.8851 23.2666 18.8851 23.9999C18.8851 24.7333 18.624 25.361 18.1018 25.8833C17.5795 26.4055 16.9518 26.6666 16.2184 26.6666ZM16.2184 18.6666C15.4851 18.6666 14.8573 18.4055 14.3351 17.8833C13.8129 17.361 13.5518 16.7333 13.5518 15.9999C13.5518 15.2666 13.8129 14.6388 14.3351 14.1166C14.8573 13.5944 15.4851 13.3333 16.2184 13.3333C16.9518 13.3333 17.5795 13.5944 18.1018 14.1166C18.624 14.6388 18.8851 15.2666 18.8851 15.9999C18.8851 16.7333 18.624 17.361 18.1018 17.8833C17.5795 18.4055 16.9518 18.6666 16.2184 18.6666ZM16.2184 10.6666C15.4851 10.6666 14.8573 10.4055 14.3351 9.88325C13.8129 9.36103 13.5518 8.73325 13.5518 7.99992C13.5518 7.26659 13.8129 6.63881 14.3351 6.11659C14.8573 5.59436 15.4851 5.33325 16.2184 5.33325C16.9518 5.33325 17.5795 5.59436 18.1018 6.11659C18.624 6.63881 18.8851 7.26659 18.8851 7.99992C18.8851 8.73325 18.624 9.36103 18.1018 9.88325C17.5795 10.4055 16.9518 10.6666 16.2184 10.6666Z" fill="white"/>
-                        </g>
-                    </svg>
-                </div>
-            </div>
-            <div class="nav_contact d-none" id="nav_contact">
-                <div class="mobile_nav d-flex_column" id="mobileNav">
-                    <div class="edit d-flex row s-gap" onclick="openEditContactOverlay(${i})">
-                        <div class="edit-img">
-                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <mask id="mask0_43661_3154" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
-                                    <rect width="24" height="24" fill="#D9D9D9" />
-                                </mask>
-                                <g mask="url(#mask0_43661_3154)">
-                                    <path d="M5 19H6.4L15.025 10.375L13.625 8.975L5 17.6V19ZM19.3 8.925L15.05 4.725L16.45 3.325C16.8333 2.94167 17.3042 2.75 17.8625 2.75C18.4208 2.75 18.8917 2.94167 19.275 3.325L20.675 4.725C21.0583 5.10833 21.2583 5.57083 21.275 6.1125C21.2917 6.65417 21.1083 7.11667 20.725 7.5L19.3 8.925ZM17.85 10.4L7.25 21H3V16.75L13.6 6.15L17.85 10.4Z" fill="#4589FF" />
-                                </g>
-                            </svg>
-                        </div>
-                        <div class="edit-text">Edit</div>
-                    </div>
-                    <div class="delete d-flex row s-gap" onclick="deleteContact(${i})">
-                        <div class="delete-img">
-                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <mask id="mask0_43661_2698" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
-                                    <rect width="24" height="24" fill="#D9D9D9" />
-                                </mask>
-                                <g mask="url(#mask0_43661_2698)">
-                                    <path d="M7 21C6.45 21 5.97917 20.8042 5.5875 20.4125C5.19583 20.0208 5 19.55 5 19V6C4.71667 6 4.47917 5.90417 4.2875 5.7125C4.09583 5.52083 4 5.28333 4 5C4 4.71667 4.09583 4.47917 4.2875 4.2875C4.47917 4.09583 4.71667 4 5 4H9C9 3.71667 9.09583 3.47917 9.2875 3.2875C9.47917 3.09583 9.71667 3 10 3H14C14.2833 3 14.5208 3.09583 14.7125 3.2875C14.9042 3.47917 15 3.71667 15 4H19C19.2833 4 19.5208 4.09583 19.7125 4.2875C19.9042 4.47917 20 4.71667 20 5C20 5.28333 19.9042 5.52083 19.7125 5.7125C19.5208 5.90417 19.2833 6 19 6V19C19 19.55 18.8042 20.0208 18.4125 20.4125C18.0208 20.8042 17.55 21 17 21H7ZM7 6V19H17V6H7ZM9 16C9 16.2833 9.09583 16.5208 9.2875 16.7125C9.47917 16.9042 9.71667 17 10 17C10.2833 17 10.5208 16.9042 10.7125 16.7125C10.9042 16.5208 11 16.2833 11 16V9C11 8.71667 10.9042 8.47917 10.7125 8.2875C10.5208 8.09583 10.2833 8 10 8C9.71667 8 9.47917 8.09583 9.2875 8.2875C9.09583 8.47917 9 8.71667 9 9V16ZM13 16C13 16.2833 13.0958 16.5208 13.2875 16.7125C13.4792 16.9042 13.7167 17 14 17C14.2833 17 14.5208 16.9042 14.7125 16.7125C14.9042 16.5208 15 16.2833 15 16V9C15 8.71667 14.9042 8.47917 14.7125 8.2875C14.5208 8.09583 14.2833 8 14 8C13.7167 8 13.4792 8.09583 13.2875 8.2875C13.0958 8.47917 13 8.71667 13 9V16Z" fill="#4589FF" />
-                                </g>
-                            </svg>
-                        </div>
-                        <div class="delete-text">Delete</div>
-                    </div>
-                </div>
-            </div>
-            <div class="arrow_return_div d-none">
-                <div class="arrow-container">
-                    <img src="./img/arrow_return.png" alt="go back" class="arrow_return" onclick="closeSelectedContactOverlay()" />
-                </div>
-            </div>
-            <div class="selected-contact-profil-top d-flex row">
-                    <div id="selected-img">
-                        <div style="background-color:${contacts[i].color}; width:90px; height:90px; font-size:1.6em;" class="contact-container-img selected">${contacts[i].initials}</div>
-                    </div>  
-                        
-                <div class="selected-contact-profil-right d-flex">
-                    <div id="selected-name" class="selected-contact-profil-name">${contacts[i].name}</div>
-                    <div class="selected-contact-profil-nav d-flex row">
-                        <div class="edit d-flex row s-gap" onclick="openEditContactOverlay(${i})">
-                            <div class="edit-img">
-                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <mask id="mask0_43661_3154" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
-                                        <rect width="24" height="24" fill="#D9D9D9" />
-                                    </mask>
-                                    <g mask="url(#mask0_43661_3154)">
-                                        <path d="M5 19H6.4L15.025 10.375L13.625 8.975L5 17.6V19ZM19.3 8.925L15.05 4.725L16.45 3.325C16.8333 2.94167 17.3042 2.75 17.8625 2.75C18.4208 2.75 18.8917 2.94167 19.275 3.325L20.675 4.725C21.0583 5.10833 21.2583 5.57083 21.275 6.1125C21.2917 6.65417 21.1083 7.11667 20.725 7.5L19.3 8.925ZM17.85 10.4L7.25 21H3V16.75L13.6 6.15L17.85 10.4Z" fill="#4589FF" />
-                                    </g>
-                                </svg>
-                            </div>
-                            <div class="edit-text">Edit</div>
-                        </div>
-                        <div class="delete d-flex row s-gap" onclick="deleteContact(${i})">
-                            <div class="delete-img">
-                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <mask id="mask0_43661_2698" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
-                                        <rect width="24" height="24" fill="#D9D9D9" />
-                                    </mask>
-                                    <g mask="url(#mask0_43661_2698)">
-                                        <path d="M7 21C6.45 21 5.97917 20.8042 5.5875 20.4125C5.19583 20.0208 5 19.55 5 19V6C4.71667 6 4.47917 5.90417 4.2875 5.7125C4.09583 5.52083 4 5.28333 4 5C4 4.71667 4.09583 4.47917 4.2875 4.2875C4.47917 4.09583 4.71667 4 5 4H9C9 3.71667 9.09583 3.47917 9.2875 3.2875C9.47917 3.09583 9.71667 3 10 3H14C14.2833 3 14.5208 3.09583 14.7125 3.2875C14.9042 3.47917 15 3.71667 15 4H19C19.2833 4 19.5208 4.09583 19.7125 4.2875C19.9042 4.47917 20 4.71667 20 5C20 5.28333 19.9042 5.52083 19.7125 5.7125C19.5208 5.90417 19.2833 6 19 6V19C19 19.55 18.8042 20.0208 18.4125 20.4125C18.0208 20.8042 17.55 21 17 21H7ZM7 6V19H17V6H7ZM9 16C9 16.2833 9.09583 16.5208 9.2875 16.7125C9.47917 16.9042 9.71667 17 10 17C10.2833 17 10.5208 16.9042 10.7125 16.7125C10.9042 16.5208 11 16.2833 11 16V9C11 8.71667 10.9042 8.47917 10.7125 8.2875C10.5208 8.09583 10.2833 8 10 8C9.71667 8 9.47917 8.09583 9.2875 8.2875C9.09583 8.47917 9 8.71667 9 9V16ZM13 16C13 16.2833 13.0958 16.5208 13.2875 16.7125C13.4792 16.9042 13.7167 17 14 17C14.2833 17 14.5208 16.9042 14.7125 16.7125C14.9042 16.5208 15 16.2833 15 16V9C15 8.71667 14.9042 8.47917 14.7125 8.2875C14.5208 8.09583 14.2833 8 14 8C13.7167 8 13.4792 8.09583 13.2875 8.2875C13.0958 8.47917 13 8.71667 13 9V16Z" fill="#4589FF" />
-                                </g>
-                            </svg>
-                        </div>
-                        <div class="delete-text">Delete</div>
-                    </div>
-                </div>
-            </div>
-            </div>
-            <div class="selected-contact-profil-bottom d-flex">
-                <div class="selected-contact-headline">Contact Information</div>
-                <div class="selected-contact-email-container d-flex s-gap">
-                    <div class="selected-contact-email-name">Email:</div>
-                    <a id="selected-mail" href="mailto:${contacts[i].email}">${contacts[i].email}</a>
-                </div>
-                <div class="selected-contact-phone-container d-flex s-gap">
-                    <div class="selected-contact-phone-name">Phone</div>
-                    <a href="${contacts[i].mobile}" id="selected-mobile" class="selected-contact-phone-adress">${contacts[i].mobile}</a>
-                </div>
-            </div>
-        </div>
-    `; */
     selectedContainer.classList.remove('d-none');
 
     setTimeout(() => {
         addBlueBackground(i);
-    }, 10); 
+    }, 10);
 }
 
+function activateInputError() {
+    const fields = [
+        { fieldId: 'email', parentId: 'email-contacts' },
+        { fieldId: 'name', parentId: 'name-contacts' },
+        { fieldId: 'mobile', parentId: 'mobile-contacts' },
+        { fieldId: 'edit-name', parentId: 'edit-name-contacts' },
+        { fieldId: 'edit-mobile', parentId: 'edit-mobile-contacts' },
+        { fieldId: 'edit-email', parentId: 'edit-email-contacts' }
+    ];
 
+    fields.forEach(({ fieldId, parentId }) => {
+        let field = docID(fieldId);
+        let parentDiv = docID(parentId);
+
+        field.addEventListener('invalid', function (event) {
+            parentDiv.classList.add('input-error');
+
+            parentDiv.addEventListener('animationend', function () {
+                parentDiv.classList.remove('input-error');
+            }, { once: true });
+        });
+
+        field.addEventListener('input', function(event) {
+            parentDiv.classList.remove('input-error');
+        });
+    });
+}
+
+/* function activateInputError() {
+    let emailField = docID('email');
+    let parentDiv = docID('email-contacts');
+    
+    emailField.addEventListener('invalid', function (event) {
+        parentDiv.classList.add('input-error');
+    
+        parentDiv.addEventListener('animationend', function () {
+            parentDiv.classList.remove('input-error');
+        }, { once: true });
+    });
+    
+    emailField.addEventListener('input', function(event) {
+        parentDiv.classList.remove('input-error');
+    });
+} */
 
 function generateContactHTML(i) {
     return `
         <div class="selected-contact-main-container">
-            <div class="open-nav-button d-none" onclick="showNav()">
+            <div class="open-nav-button d-none mybtn" onclick="showNav()">
                 <div class="add-contact-button-text">Add new contact</div>
                 <div class="add-contact-button-img">
                     <svg width="33" height="32" viewBox="0 0 33 32" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -366,7 +308,7 @@ function generateContactHTML(i) {
                 </div>
             </div>
             <div class="arrow_return_div d-none">
-                <div class="arrow-container">
+                <div class="my-arrow-container mybtn">
                     <img src="./img/arrow_return.png" alt="go back" class="arrow_return" onclick="closeSelectedContactOverlay()" />
                 </div>
             </div>
@@ -477,7 +419,7 @@ function renderEditOverlay(i) {
         <div class="edit-contact_main-container">
             <div class="edit-area_top-area d-flex_column">
                 <div class="close-container d-flex_column">
-                    <div onclick="closeEditContactOverlay()" class="close-edit-contacts">
+                    <div onclick="closeEditContactOverlay()" class="close-edit-contacts mybtn">
                         <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <mask id="mask0_19024_6446" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="4" y="4" width="24" height="24">
                                 <rect x="4" y="4" width="24" height="24" fill="#D9D9D9" />
@@ -509,7 +451,7 @@ function renderEditOverlay(i) {
             </div>
             <div class="edit-area_bottom-area d-flex_column">
                 <form id="edit-contact-form" onsubmit="editContact(${i}); return false;">
-                    <div class="name-edit-contacts d-flex_row input-container">
+                    <div id="edit-name-contacts" class="name-edit-contacts d-flex_row input-container">
                         <input id="edit-name" placeholder="Name" name="name" pattern="^(?=.*[A-Za-zäöüÄÖÜß]{3}).*$"
                             title="Mindestens drei Buchstaben" class="name-input" type="text" value="${contacts[i]['name']}" required>
                         <div class="name-contacts-img target">
@@ -523,8 +465,8 @@ function renderEditOverlay(i) {
                             </svg>
                         </div>
                     </div>
-                    <div class="email-edit-contacts d-flex_row input-container">
-                        <input id="edit-email" name="email" placeholder="Email" class="email-input" type="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" title="Bitte geben Sie eine gültige E-Mail-Adresse ein" value="${contacts[i]['email']}" required>
+                    <div id="edit-email-contacts" class="email-edit-contacts d-flex_row input-container">
+                    <input id="edit-email" name="email" placeholder="Email" class="email-input" type="email" pattern="^[^\s@]+@[^\s@]+\.[^\s@]+$" title="Bitte geben Sie eine gültige E-Mail-Adresse ein" value="${contacts[i]['email']}" required>
                         <div class="email-contacts-img target">
                             <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <mask id="mask0_43661_2816" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="25">
@@ -536,8 +478,9 @@ function renderEditOverlay(i) {
                             </svg>
                         </div>
                     </div>
-                    <div class="phone-edit-contacts d-flex_row input-container">
-                        <input id="edit-mobile" name="tel" pattern="\\+?[0-9\s\-\/]{7,15}" title="Geben Sie eine gültige Telefonnummer ein" placeholder="Phone" class="phone-input" type="tel" value="${contacts[i]['mobile']}">
+                    <div id="edit-mobile-contacts" class="phone-edit-contacts d-flex_row input-container">
+                    <input id="edit-mobile" name="tel" pattern="\\+?[0-9\\s\\-]{7,15}" title="Geben Sie eine gültige Telefonnummer ein" placeholder="Phone" class="phone-input" type="tel" value="${contacts[i]['mobile']}">
+
                         <div class="phone-contacts-img target">
                             <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <mask id="mask0_43661_2823" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="25">
@@ -550,10 +493,10 @@ function renderEditOverlay(i) {
                         </div>
                     </div>
                     <div id="edit-button-container" class="button_container d-flex_row">
-                        <div onclick="clearEditForm()" id="delete_in_edit" class="blue-button-container d-flex_row">
+                        <div onclick="clearEditForm()" id="delete_in_edit" class="blue-button-container d-flex_row mybtn">
                             <div class="blue-button-text">Delete</div>
                         </div>
-                        <button type="submit" id="edit_in_edit" class="save-button-container d-flex_row">
+                        <button type="submit" id="edit_in_edit" class="mybtn save-button-container d-flex_row">
                             <div class="save-button-text">Save</div>
                             <div class="save-button-hook">
                                 <svg width="25" height="25" viewBox="0 0 25 25" fill="black" xmlns="http://www.w3.org/2000/svg">
@@ -574,6 +517,7 @@ function renderEditOverlay(i) {
 
     docID('overlay_edit-contact').innerHTML = overlayHTML;
     docID('overlay_edit-contact').classList.remove('d-none');
+    activateInputError();
 }
 
 /* async function editContact(i) {
@@ -732,3 +676,4 @@ function toggleMobileNav() {
         mobileNav.classList.add('show');
     }
 }
+
